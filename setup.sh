113c4a1f1889f1a4bd7cffb26d5e81f908c15cad
Brijesh Poojary <103484184+BijjuXD@users.noreply.github.com>
2026-01-18 09:01:31 +0530

    Change repository source from pterodactyl to reviactyl

--- setup.sh
+++ setup.sh
@@ -3,32 +3,32 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 CERTS_DIR="$SCRIPT_DIR/docker/certificates"
 CODE_DIR="$SCRIPT_DIR/code"
 
-for REPO in "panel" "documentation" "wings"
+for REPO in "panel" "wings"
 do
   if [ ! -d "$CODE_DIR/$REPO" ]; then
-    git clone https://github.com/pterodactyl/$REPO.git "$CODE_DIR/$REPO"
+    git clone https://github.com/reviactyl/$REPO.git "$CODE_DIR/$REPO"
   else
     echo "$REPO repository already cloned into: $CODE_DIR/$REPO"
   fi
 done
 
 mkcert -install
-mkcert pterodactyl.test wings.pterodactyl.test minio.pterodactyl.test s3.minio.pterodactyl.test
+mkcert reviactyl.test wings.reviactyl.test minio.reviactyl.test s3.minio.reviactyl.test
 
 # Because we're doing Docker-in-Docker we actually need these paths to line
 # up correctly with the host system.
 sudo mkdir -p /var/lib/pterodactyl
 sudo chown $(id -u):$(id -g) /var/lib/pterodactyl
 
-mv -v *pterodactyl.test*-key.pem docker/certificates/pterodactyl.test-key.pem || exit 1
-mv -v *pterodactyl.test*.pem docker/certificates/pterodactyl.test.pem || exit 1
+mv -v *reviactyl.test*-key.pem docker/certificates/reviactyl.test-key.pem || exit 1
+mv -v *reviactyl.test*.pem docker/certificates/reviactyl.test.pem || exit 1
 cp -v "$(mkcert -CAROOT)/rootCA.pem" docker/certificates/root_ca.pem || exit 1
 
 echo ""
 if [ ! -f "/etc/hosts" ]; then
   echo "no system hosts file found, please manually configure your system"
 else
-  for DOMAIN in "pterodactyl.test" "wings.pterodactyl.test" "minio.pterodactyl.test" "s3.minio.pterodactyl.test"
+  for DOMAIN in "reviactyl.test" "wings.reviactyl.test" "minio.reviactyl.test" "s3.minio.reviactyl.test"
   do
     ESCAPED_DOMAIN=$(echo $DOMAIN | sed "s/\./\\\./g")
     if ! grep -q -E "127\.0\.0\.1\s+$ESCAPED_DOMAIN\s*$" /etc/hosts; then
